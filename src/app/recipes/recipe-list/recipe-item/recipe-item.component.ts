12ea9f2eba9d1e2bd9edb3637a43802af5a941f7
anask601 <anask60155@gmail.com>
2022-03-26 20:26:36 +0530

    feat: added recipe list and recipe item

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,3 @@
-import { Recipe } from './../../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,19 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css'],
 })
 export class RecipeItemComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
-    ),
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
-    ),
-  ];
-
   constructor() {}
 
   ngOnInit(): void {}
