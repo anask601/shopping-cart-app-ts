d2c8cef877d232097a765cc0d5f942b359b836f3
anask601 <anask60155@gmail.com>
2022-03-30 13:53:02 +0530

    feat: navigation is add  and data binding

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-
+  @Output() featuredSelected = new EventEmitter<string>();
+  //  note: data tranfer from child to parent
+  OnSelect(feature: string) {
+    if (feature === 'recipe') {
+      this.featuredSelected.emit(feature);
+    } else if (feature === 'shopping-list') {
+      this.featuredSelected.emit(feature);
+    }
+  }
 }
