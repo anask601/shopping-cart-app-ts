261f94ce236115007989bc0a77ae0b3542efaa88
anask601 <anask60155@gmail.com>
2022-03-31 08:08:54 +0530

    feat: user to add ingredents

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Ingredients } from './../../shared/ingredients.model';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  EventEmitter,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInputRef') nameInputRef!: ElementRef;
+  @ViewChild('amountInputRef') amountInputRef!: ElementRef;
+  // ingredients = new EventEmitter<{ name: string; amount: number }>();
+  @Output() ingredients = new EventEmitter<Ingredients>();
 
-  constructor() { }
-
-  ngOnInit() {
+  OnAdd() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    console.log(ingName + ingAmount);
+    const newIngredients = new Ingredients(ingName, ingAmount);
+    this.ingredients.emit(newIngredients);
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,21 +1,24 @@
+import { Ingredients } from './../shared/ingredients.model';
 import { Component, OnInit } from '@angular/core';
 
-import { Ingredient } from '../shared/ingredient.model';
+// import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
+  ingredients: Ingredients[] = [
+    new Ingredients('Apples', 5),
+    new Ingredients('Tomatoes', 10),
   ];
 
-  constructor() { }
-
-  ngOnInit() {
+  OnAdd(event: Ingredients) {
+    this.ingredients.push(event);
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
